e03eb4b80e9f94eb385cbadf45cee3ba43f4e593
Dan Milstein <milstein.dan@gmail.com>
2025-05-27 07:33:15 -0400

    Initial commit w/ skeleton of files to start

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil
+  .
+  ((org-roam-directory
+    .
+    "/Users/danmil/Dropbox (Personal)/Projects/TechInvestBook/org-roam-tibook")
+   (org-roam-db-location . "/Users/danmil/.emacs.d/org-roam-tibook.db"))))
